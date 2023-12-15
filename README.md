234cc73169f6383fb5a2ef4d9f58e163df652060
Fabrício Maciel <31020372+fabrimaciel@users.noreply.github.com>
2023-12-15 10:21:32 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Colosoft Event Bus UnitOfWork
+
+Project with the definition of colosoft's event bus logging.
+
+
+| Package | NuGet | Downloads |
+| ------- | ------------ | --------- |
+| [Colosoft.EventBus.UnitOfWork](https://www.nuget.org/packages/Colosoft.EventBus.UnitOfWork/) | [![Colosoft.EventBus.UnitOfWork](https://img.shields.io/nuget/v/Colosoft.EventBus.UnitOfWork.svg)](https://www.nuget.org/packages/Colosoft.EventBus.UnitOfWork/) | [![Colosoft.EventBus.UnitOfWork](https://img.shields.io/nuget/dt/Colosoft.EventBus.UnitOfWork.svg)](https://www.nuget.org/packages/Colosoft.EventBus.UnitOfWork/) |
+
+
---
+++ src/Colosoft.EventBus.UnitOfWork/Behaviors/UnitOfWorkHandlerBehavior.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Colosoft.EventBus.Behaviors
+{
+    public class UnitOfWorkHandlerBehavior<TIntegrationEvent> : IPipelineHandlerBehavior<TIntegrationEvent>
+    {
+        protected IUnitOfWorkProvider UnitOfWorkProvider { get; }
+        private readonly ILogger<UnitOfWorkHandlerBehavior<TIntegrationEvent>> logger;
+
+        public UnitOfWorkHandlerBehavior(
+            IUnitOfWorkProvider unitOfWorkProvider,
+            ILogger<UnitOfWorkHandlerBehavior<TIntegrationEvent>> logger)
+        {
+            this.UnitOfWorkProvider = unitOfWorkProvider ?? throw new ArgumentNullException(nameof(unitOfWorkProvider));
+            this.logger = logger ?? throw new ArgumentException(nameof(ILogger));
+        }
+
+        protected virtual Task PublishEventsThroughEventBusAsync(Guid transactionId, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        protected virtual IUnitOfWork CreateUnitOfWork()
+        {
+            return this.UnitOfWorkProvider.Create();
+        }
+
+        public async Task Handle(TIntegrationEvent @event, IIntegrationEventContext context, IntegrationEventHandlerDelegate next, CancellationToken cancellationToken)
+        {
+            var typeName = @event.GetGenericTypeName();
+
+            try
+            {
+                if (this.UnitOfWorkProvider.GetCurrent() != null)
+                {
+                    await next(cancellationToken);
+                }
+
+                using (var unitOfWork = this.CreateUnitOfWork())
+                {
+                    var transactionId = unitOfWork.Id;
+
+                    try
+                    {
+                        using (this.logger.BeginScope(new List<Tuple<string, object>> { new Tuple<string, object>("TransactionContext", transactionId) }))
+                        {
+                            this.logger.LogInformation("Begin transaction {TransactionId} for {CommandName} ({@Command})", transactionId, typeName, @event);
+
+                            await next(cancellationToken);
+
+                            this.logger.LogInformation("Commit transaction {TransactionId} for {CommandName}", transactionId, typeName);
+
+                            await unitOfWork.CommitAsync(cancellationToken);
+                        }
+                    }
+                    catch
+                    {
+                        await unitOfWork.RollbackAsync(cancellationToken);
+                        throw;
+                    }
+
+                    await this.PublishEventsThroughEventBusAsync(transactionId, cancellationToken);
+                }
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, "Error Handling transaction for {CommandName} ({@Command})", typeName, @event);
+
+                throw;
+            }
+        }
+    }
+}
